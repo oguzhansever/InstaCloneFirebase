ff1f1e7f0442674374dc586608ec159ae27050c0
oguzhansever <103364929+oguzhansever@users.noreply.github.com>
2022-08-13 01:01:01 +0300

    Add files via upload

---
+++ InstaCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Oğuzhan Sever on 9.08.2022.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+            
+        } catch {
+            print("error")
+            
+        }
+       
+        
+        
+    }
+    
+}
